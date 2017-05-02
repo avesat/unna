ea027fdff16c09f87f311dbc30c1bc33ca52bec4
Oleksii <okolka@cisco.com>
2017-05-02 12:50:14 +0300

    Added Catalog-List stub structure

---
+++ src/app/core/components/catalog/catalog-sublist/catalog-sublist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-sublist',
+  templateUrl: './catalog-sublist.component.html',
+  styleUrls: ['./catalog-sublist.component.css']
+})
+export class CatalogSublistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
